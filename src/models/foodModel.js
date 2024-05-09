5ba83300ca07b44cb69f2229b0c30879bc47f8ae
HOSSAIN\meraj <merajhossain29@gmail.com>
2024-05-10 00:39:18 +0600

    --

--- src/models/foodModel.js
+++ src/models/foodModel.js
@@ -4,8 +4,8 @@ const DatabaseScheme = mongoose.Schema({
     foodName: { type: String, required: true },
     foodCode: { type: String, unique: true, required: true },
     foodImageUrl: { type: String, required: true },
-    foodCategory: { type: String, required: true },
-    qty: { type: String, required: true },
+    foodCategory: { type: String },
+    qty: { type: String },
     price: { type: String, required: true }
 }, { versionKey: 'true' });
 
